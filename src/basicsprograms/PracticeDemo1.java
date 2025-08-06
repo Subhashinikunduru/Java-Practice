bc701d01acdf8a852afc56a401acbc07a991ca32
Subhashini Kunduru <subhashinikunduru99@gmail.com>
2025-08-06 19:20:03 +0530

    Today Pratice 6-08-2025

---
+++ src/basicsprograms/PracticeDemo1.java
@@ -0,0 +1,15 @@
+package basicsprograms;
+
+public class PracticeDemo1 {
+
+	public static void main(String[] args) {
+System.out.println("practice git commands");
+//git status
+//git add .
+//git commit -m"today practice"
+//git push origin main
+//git status
+//git log
+	}
+
+}
